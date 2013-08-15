82622550dd29a835b40700b7a3915b7cfbd293c4
TomiCode <tomicode@gmail.com>
2013-08-15 22:12:33 +0200

    Initial Commit.

---
+++ Net_Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Net_Client
+{
+    class Program
+    {
+        static short PORT_NUMBER = 15224;
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Net_Server/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Net_Server
+{
+    class Program
+    {
+        static short PORT_NUMBER = 15224;
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
